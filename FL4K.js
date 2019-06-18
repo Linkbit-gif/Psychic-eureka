1618a19489160fa4d09b2d73f8bbe920cc1cd018
Linkbit-gif <51913413+Linkbit-gif@users.noreply.github.com>
2019-06-19 00:40:40 +0100

    Add files via upload

---
+++ FL4K.js
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-16">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bootstrap - Prebuilt Layout</title>
+
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="Assignment Content/CSS/styles/mystyle3.css" rel="stylesheet" type="text/css">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https:@media only screen and (min-width: 320px) {
+
+
+
+  /* For non-retina based devices that have a smaller screen */
+
+
+
+}
+
+
+
+@media
+
+only screen and (-webkit-min-device-pixel-ratio: 2)      and (min-width: 320px),
+
+only screen and (   min--moz-device-pixel-ratio: 2)      and (min-width: 320px),
+
+only screen and (     -o-min-device-pixel-ratio: 2/1)    and (min-width: 320px),
+
+only screen and (        min-device-pixel-ratio: 2)      and (min-width: 320px),
+
+only screen and (                min-resolution: 192dpi) and (min-width: 320px),
+
+only screen and (                min-resolution: 2dppx)  and (min-width: 320px) { 
+
+
+
+  /* Retina enabled devices with smaller screen */
+
+
+
+}
+
+
+
+@media only screen and (min-width: 768px) {
+
+
+
+  /*Non- retina based devices with medium screen size */
+
+
+
+}
+
+
+
+@media
+
+only screen and (-webkit-min-device-pixel-ratio: 2)      and (min-width: 768px),
+
+only screen and (   min--moz-device-pixel-ratio: 2)      and (min-width: 768px),
+
+only screen and (     -o-min-device-pixel-ratio: 2/1)    and (min-width: 768px),
+
+only screen and (        min-device-pixel-ratio: 2)      and (min-width: 768px),
+
+only screen and (                min-resolution: 192dpi) and (min-width: 768px),
+
+only screen and (                min-resolution: 2dppx)  and (min-width: 768px) { 
+
+
+
+  /* Retina devices with medium screen size */
+
+
+
+}
+
+
+
+@media only screen and (min-width: 1200px) {
+
+
+
+  /* Non-retina devices with large screen sizes */
+
+
+
+}
+
+
+
+@media
+
+only screen and (-webkit-min-device-pixel-ratio: 2)      and (min-width: 1200px),
+
+only screen and (   min--moz-device-pixel-ratio: 2)      and (min-width: 1200px),
+
+only screen and (     -o-min-device-pixel-ratio: 2/1)    and (min-width: 1200px),
+
+only screen and (        min-device-pixel-ratio: 2)      and (min-width: 1200px),
+
+only screen and (                min-resolution: 192dpi) and (min-width: 1200px),
+
+only screen and (                min-resolution: 2dppx)  and (min-width: 1200px) { 
+
+
+
+  /* Retina devices with large screen sizes */
+
+
+
+}//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<nav class="nav<picture alt="description of image">
+
+
+
+  <!-- Default low resolution image -->
+
+  <source src="small.jpg">
+
+
+
+  <!-- Medium resolution image -->
+
+  <source src="medium.jpg" media="(min-width: 768px)">
+
+
+
+  <!--High resolution Image -->
+
+  <source src="large.jpg" media="(min-width: 1200px)">
+
+
+
+  <!-- Fallback image incase the custom picture element is not supported -->
+
+  <img src="small.jpg" alt="description of image">
+
+
+
+</picture>bar navbar-default">
+  <div @media only screen and (min-width: 320px) {
+
+
+
+  /* For non-retina based devices that have a smaller screen */
+
+
+
+}
+
+
+
+@media
+
+only screen and (-webkit-min-device-pixel-ratio: 2)      and (min-width: 320px),
+
+only screen and (   min--moz-device-pixel-ratio: 2)      and (min-width: 320px),
+
+only screen and (     -o-min-device-pixel-ratio: 2/1)    and (min-width: 320px),
+
+only screen and (        min-device-pixel-ratio: 2)      and (min-width: 320px),
+
+only screen and (                min-resolution: 192dpi) and (min-width: 320px),
+
+only screen and (                min-resolution: 2dppx)  and (min-width: 320px) { 
+
+
+
+  /* Retina enabled devices with smaller screen */
+
+
+
+}
+
+
+
+@media only screen and (min-width: 768px) {
+
+
+
+  /*Non- retina based devices with medium screen size */
+
+
+
+}
+
+
+
+@media
+
+only screen and (-webkit-min-device-pixel-ratio: 2)      and (min-width: 768px),
+
+only screen and (   min--moz-device-pixel-ratio: 2)      and (min-width: 768px),
+
+only screen and (     -o-min-device-pixel-ratio: 2/1)    and (min-width: 768px),
+
+only screen and (        min-device-pixel-ratio: 2)      and (min-width: 768px),
+
+only screen and (                min-resolution: 192dpi) and (min-width: 768px),
+
+only screen and (                min-resolution: 2dppx)  and (min-width: 768px) { 
+
+
+
+  /* Retina devices with medium screen size */
+
+
+
+}
+
+
+
+@media only screen and (min-width: 1200px) {
+
+
+
+  /* Non-retina devices with large screen sizes */
+
+
+
+}
+
+
+
+@media
+
+only screen and (-webkit-min-device-pixel-ratio: 2)      and (min-width: 1200px),
+
+only screen and (   min--moz-device-pixel-ratio: 2)      and (min-width: 1200px),
+
+only screen and (     -o-min-device-pixel-ratio: 2/1)    and (min-width: 1200px),
+
+only screen and (        min-device-pixel-ratio: 2)      and (min-width: 1200px),
+
+only screen and (                min-resolution: 192dpi) and (min-width: 1200px),
+
+only screen and (                min-resolution: 2dppx)  and (min-width: 1200px) { 
+
+
+
+  /* Retina devices with large screen sizes */
+
+
+
+}class="container-fluid"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#defaultNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      <a class="navbar-brand" href="#">Brand</a></div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="defaultNavbar1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid --> 
+</nav>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <h1 class="text-center">Bootstrap with Dreamweaver</h1>
+    </div>
+  </div>
+  <hr>
+</div>
+<div class="container">
+  <div class="row text-center">
+    <div class="col-md-6 col-md-offset-3">Click outside the blue container to select this <strong>row</strong>. Columns are always contained within a row. <strong>Rows are indicated by a dashed grey line and rounded corners</strong>. </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="text-justify col-sm-4"> Click here to select this<strong> column.</strong> Always place your content within a column. Columns are indicated by a dashed blue line. </div>
+    <div class="col-sm-4 text-justify"> You can <strong>resize a column</strong> using the handle on the right. Drag it to increase or reduce the number of columns.</div>
+    <div class="col-sm-4 text-justify"> You can <strong>offset a column</strong> using the handle on the left. Drag it to increase or reduce the offset. </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="text-center col-md-12">
+      <div class="well"><strong> Easily build your page using the Bootstrap components from the Insert panel.</strong></div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-4 text-center">
+      <h4>Adding <strong>Buttons</strong></h4>
+      <p>Quickly add buttons to your page by using the button component in the insert panel. </p>
+      <button type="button" class="btn btn-info btn-sm">Info Button</button>
+      <button type="button" class="btn btn-success btn-sm">Success Button</button>
+    </div>
+    <div class="text-center col-sm-4">
+      <h4>Adding <strong>Labels</strong></h4>
+      <p>Using the insert panel, add labels to your page by using the label component.</p>
+      <span class="label label-warning">Info Label</span><span class="label label-danger">Danger Label</span> </div>
+    <div class="text-center col-sm-4">
+      <h4>Adding <strong>Glyphicons</strong></h4>
+      <p>You can also add glyphicons to your page from within the insert panel.</p>
+      <div class="row">
+        <div class="col-xs-4"><span class="glyphicon glyphicon-.rotate (@deg) {
+	-webkit-transform: rotate(@deg);
+	-moz-transform: 	 rotate(@deg);
+	-ms-transform: 		 rotate(@deg);
+	-o-transform: 		 rotate(@deg);
+}menu-hamburger" aria-hidden="true"></span></div>
+        <div class="col-xs-4"><span class="glyphicon glyphicon-home" aria-hidden="true"> </span> </div>
+        <div class="col-xs-4"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></div>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="text-center col-md-6 col-md-offset-3">
+      <h4>Footer </h4>
+      <p>Copyright &copy; 2015 &middot; All Rights Reserved &middot; <a href="http://yourwebsite.com/" >My Website</a></p>
+    </div>
+  </div>
+  <hr>
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.2.min.js"></script>
+
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
